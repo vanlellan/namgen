347490872b32627705466e8dc9ebc77a0906920e
Randall Evan McClellan <mcclell5@illnois.edu>
2017-04-09 23:30:22 -0400

    added depth=2 code
    -depth one and up can probably be implemented in a single function
    -depth zero might require a special case

--- namgen.py
+++ namgen.py
@@ -4,8 +4,8 @@
 
 #TO DO
 #DONE	implement command line argument handler
-#	implement choice of markov depth
-#	implement namgen loop, options to quit, save, or next after each name
+#0-2	implement choice of markov depth
+#	implement loop, options to quit, save, or next after each name
 
 
 import random
@@ -13,13 +13,15 @@ from optparse import OptionParser
 
 
 parser = OptionParser('Usage: %prog [options]')
-parser.add_option('-d', '--depth', type = 'int', dest = 'depth', help = 'Markov Chain depth [default=%default]', default = 1)
+parser.add_option('-d', '--depth', type = 'int', dest = 'depth', help = 'Markov Chain depth (0,1,2) [default=%default]', default = 1)
 parser.add_option('-i', '--infile', type = 'string', dest = 'infilename', help = 'Input file with list of names', default = '')
 
 
 
 (options, args) = parser.parse_args()
 
+if options.depth not in (0,1,2):
+	parser.parse_args(["--help"])
 
 
 
@@ -60,6 +62,31 @@ if options.depth == 1:
 	name = '^'
 	while name[-1] != '$':
 		name = name + random.choice(cor1[name[-1]])
+	
+	#print name
+	print name[1:-1]
+
+
+if options.depth == 2:
+	cor2 = {}
+	startchar = ''
+	#Read in Library of names
+	with open(options.infilename, 'r') as infile:
+		for line in infile:
+			if line[0] != '#':
+				line = '^'+line.rstrip()+'$'
+				for i,let in enumerate(line):
+					if i==1:
+						startchar = startchar+let
+					if i>=2:
+						try:
+							cor2[line[i-2:i]] += let
+						except KeyError:
+							cor2[line[i-2:i]] = let
+	#Generate name
+	name = '^'+random.choice(startchar)
+	while name[-1] != '$':
+		name = name + random.choice(cor2[name[-2:]])
 	
 	#print name
 	print name[1:-1]
