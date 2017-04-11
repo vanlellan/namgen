f5c9cdcad98b0d06e0a2ddfefa08a2ac8aa5333f
Randall Evan McClellan <mcclell5@illnois.edu>
2017-04-10 23:12:10 -0400

    added loop and 'while running' commands

--- namgen.py
+++ namgen.py
@@ -6,7 +6,7 @@
 #DONE	implement command line argument handler
 #0-2	implement choice of markov depth
 #	implement loop, options to quit, save, or next after each name
-
+#	write function for depth 1 and up
 
 import random
 from optparse import OptionParser
@@ -15,6 +15,7 @@ from optparse import OptionParser
 parser = OptionParser('Usage: %prog [options]')
 parser.add_option('-d', '--depth', type = 'int', dest = 'depth', help = 'Markov Chain depth (0,1,2) [default=%default]', default = 1)
 parser.add_option('-i', '--infile', type = 'string', dest = 'infilename', help = 'Input file with list of names', default = '')
+parser.add_option('-o', '--outfile', type = 'string', dest = 'outfilename', help = 'Output file for saving names', default = '')
 
 
 
@@ -25,70 +26,85 @@ if options.depth not in (0,1,2):
 
 
 
+prevname = ''
+keepOnKeepingOn = True
+print "Enter for another name, s+Enter to save previous name, q+Enter to quit..."
+while keepOnKeepingOn:
 
-allchars = '^abcdefghijklmnopqrstuvwxyz$'
+	command = raw_input()
 
-if options.depth == 0:
-	#Read in names
-	cor0 = ''
-	with open(options.infilename, 'r') as infile:
-		for line in infile:
-			if line[0] != '#':
-				line = line.rstrip()+'$'
-				cor0 = cor0 + line
-	#Generate name
-	name = '^'
-	while name[-1] != '$':
-		name = name + random.choice(cor0)
+	if command == 'q':
+		keepOnKeepingOn = False
 
-	#print name
-	print name[1:-1]
+	if command == 's':
+		with open(options.outfilename, 'a') as outfile:
+			outfile.write(prevname+'\n')
+		print "Saved \'"+prevname+"\' to "+options.outfilename
 
-
-if options.depth == 1:
-	cor1 = {}
-	#Read in Library of names
-	with open(options.infilename, 'r') as infile:
-		for line in infile:
-			if line[0] != '#':
-				line = '^'+line.rstrip()+'$'
-				for i,let in enumerate(line):
-					if i!=0:
-						try:
-							cor1[line[i-1]] += let
-						except KeyError:
-							cor1[line[i-1]] = let
-	#Generate name
-	name = '^'
-	while name[-1] != '$':
-		name = name + random.choice(cor1[name[-1]])
+	allchars = '^abcdefghijklmnopqrstuvwxyz$'
 	
-	#print name
-	print name[1:-1]
-
-
-if options.depth == 2:
-	cor2 = {}
-	startchar = ''
-	#Read in Library of names
-	with open(options.infilename, 'r') as infile:
-		for line in infile:
-			if line[0] != '#':
-				line = '^'+line.rstrip()+'$'
-				for i,let in enumerate(line):
-					if i==1:
-						startchar = startchar+let
-					if i>=2:
-						try:
-							cor2[line[i-2:i]] += let
-						except KeyError:
-							cor2[line[i-2:i]] = let
-	#Generate name
-	name = '^'+random.choice(startchar)
-	while name[-1] != '$':
-		name = name + random.choice(cor2[name[-2:]])
+	if options.depth == 0 and command == '':
+		#Read in names
+		cor0 = ''
+		with open(options.infilename, 'r') as infile:
+			for line in infile:
+				if line[0] != '#':
+					line = line.rstrip()+'$'
+					cor0 = cor0 + line
+		#Generate name
+		name = '^'
+		while name[-1] != '$':
+			name = name + random.choice(cor0)
 	
-	#print name
-	print name[1:-1]
-
+		#print name
+		print name[1:-1]
+	
+	
+	if options.depth == 1 and command == '':
+		cor1 = {}
+		#Read in Library of names
+		with open(options.infilename, 'r') as infile:
+			for line in infile:
+				if line[0] != '#':
+					line = '^'+line.rstrip()+'$'
+					for i,let in enumerate(line):
+						if i!=0:
+							try:
+								cor1[line[i-1]] += let
+							except KeyError:
+								cor1[line[i-1]] = let
+		#Generate name
+		name = '^'
+		while name[-1] != '$':
+			name = name + random.choice(cor1[name[-1]])
+		
+		#print name
+		print name[1:-1]
+	
+	
+	if options.depth == 2 and command == '':
+		cor2 = {}
+		startchar = ''
+		#Read in Library of names
+		with open(options.infilename, 'r') as infile:
+			for line in infile:
+				if line[0] != '#':
+					line = '^'+line.rstrip()+'$'
+					for i,let in enumerate(line):
+						if i==1:
+							startchar = startchar+let
+						if i>=2:
+							try:
+								cor2[line[i-2:i]] += let
+							except KeyError:
+								cor2[line[i-2:i]] = let
+		#Generate name
+		name = '^'+random.choice(startchar)
+		while name[-1] != '$':
+			name = name + random.choice(cor2[name[-2:]])
+		
+		#print name
+		print name[1:-1]
+		prevname = name[1:-1]
+	
 
